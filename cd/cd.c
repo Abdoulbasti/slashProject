0487ae572566f7151bb2348e33d74aa6dfdaac24
PUSKAS Matyas <matyaspuskas76@protonmail.com>
2022-11-23 16:44:26 +0100

    version complète de cd

---
+++ cd/cd.c
@@ -0,0 +1,197 @@
+#include <string.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "cd.h"
+#include "../pwd/pwd.h"
+
+char dernier_sym[PATH_MAX] = "\0";
+
+//insère dans "res" un string contenant les caractères jusqu'au premier "/" inclus de "chemin"
+void prochain_dossier(char * nom_dossier, char * chemin){
+	nom_dossier[0] = '\0';
+	int len = strlen(chemin);
+	int i;
+	for(i = 0; i<len && chemin[i]!='/'; i++){
+		nom_dossier[i] = chemin[i];
+	}
+	nom_dossier[i] = '\0';
+}
+
+//enlève les caractères jusqu'au premier "/" inclus
+void enlever_premier_dossier(char * chemin){
+	char tmp[PATH_MAX];
+	tmp[0] = '\0';
+	int len = strlen(chemin);
+	int i;
+	for(i = 0; i<len && chemin[i]!='/'; i++){}
+	sprintf(tmp, "%s", chemin);
+	if(i == len){
+		chemin[0] = '\0';
+	}
+	else{
+		sprintf(chemin, "%s", tmp+i+1);
+	}
+}
+
+//enlève les charactères à partir du dernier "/"
+void enlever_dernier_dossier(char * chemin){
+	int len = strlen(chemin);
+	int i;
+	for(i = len; i>0 && chemin[i]!='/'; i--){}
+	chemin[i]='\0';
+}
+
+int cd(int argc, char **argv){
+	if(strcmp(dernier_sym, "\0") == 0){
+		sprintf(dernier_sym, "%s", getenv("PWD"));
+	}
+	if(argc == 0){
+		int fd = open(getenv("HOME"), O_RDONLY, 0666);
+		if(fd < 0){
+			perror(NULL);
+			exit(1);
+		}
+		close(fd);
+		setenv("PWD", getenv("HOME"), 1);
+	}
+	else if(argc == 1 && strcmp(argv[0],"-")==0){
+		//ouvre le dernier répertoire puis échange les deux chemins symboliques
+		int fd = open(dernier_sym, O_RDONLY, 0666);
+		if(fd < 0){
+			perror(NULL);
+			exit(1);
+		}
+		close(fd);
+		char tmp[PATH_MAX];
+		sprintf(tmp, "%s", dernier_sym);
+		sprintf(dernier_sym, "%s", getenv("PWD"));
+		setenv("PWD", tmp, 1);
+	}
+	//chemin interprété de manière logique
+	else if(argc == 1 || (argc == 2 && strcmp(argv[0],"-L")==0)){
+		char param[PATH_MAX];
+		if(argc == 1){
+			sprintf(param, "%s", argv[0]);
+		}
+		else{
+			sprintf(param, "%s", argv[1]);
+		}
+		//stocke le nouveau chemin symbolique. Il sera écrit dans la variable "pwd" si il est valide
+		char tmp[PATH_MAX];
+
+		//ouverture du dossier à partir duquel le parcours sera effectué
+		int fd;
+		//si c'est une référence relative, le parcours commence par le répertoire de travail
+		if(param[0] != '/'){
+			fd = open(getenv("PWD"), O_RDONLY, 0666);
+			if(fd < 0){
+				perror(NULL);
+				exit(1);
+			}
+			//comme le parcours commence par le répertoire de travail courant, c'est la valeur de $PWD qui est copiée dans tmp
+			sprintf(tmp, "%s", getenv("PWD"));
+		}
+		else{
+			fd = open("/", O_RDONLY, 0666);
+			if(fd < 0){
+				perror(NULL);
+				exit(1);
+			}
+			//on enlève le premier "/" de param
+			enlever_premier_dossier(param);
+		}
+		
+		//nom du premier dossier dans le chemin à parcourir
+		char nom_dossier[PATH_MAX];
+
+		//boucle de parcours
+		while(param[0]!='\0'){
+			prochain_dossier(nom_dossier, param);
+
+			int fd_sous = openat(fd, nom_dossier, O_RDONLY,  0666);
+			if(fd_sous < 0){
+				char * nargv[] = {"cd", "-P", param};
+				return cd(3, nargv);
+			}
+			else{
+				close(fd);
+				fd = fd_sous;
+			}
+
+			if(strcmp(nom_dossier, "..") == 0){
+				enlever_dernier_dossier(tmp);
+			}
+			else if(strcmp(nom_dossier, ".") != 0){
+				strcat(tmp, "/");
+				strcat(tmp, nom_dossier);
+			}
+			enlever_premier_dossier(param);
+		}
+		close(fd);
+		sprintf(dernier_sym, "%s", getenv("PWD"));
+		setenv("PWD", tmp, 1);
+	}
+	//chemin interprété de manière physique
+	else if(argc == 2 && strcmp(argv[0],"-P")==0){
+		char param[PATH_MAX];
+		sprintf(param, "%s", argv[1]);
+
+		int fd;
+		//ouverture du dossier à partir duquel le parcours sera effectué
+		//si c'est une référence relative, le parcours commence par le répertoire de travail
+		if(param[0] != '/'){
+			fd = open(getenv("PWD"), O_RDONLY, 0666);
+			if(fd < 0){
+				perror(NULL);
+				exit(1);
+			}
+		}
+		else{
+			fd = open("/", O_RDONLY, 0666);
+			if(fd < 0){
+				perror(NULL);
+				exit(1);
+			}
+			//on enlève le premier "/" de param
+			enlever_premier_dossier(param);
+		}
+		
+		//nom du premier dossier dans le chemin à parcourir
+		char nom_dossier[PATH_MAX];
+
+		//boucle de parcours
+		while(param[0]!='\0'){
+			prochain_dossier(nom_dossier, param);
+			
+			int fd_sous = openat(fd, nom_dossier, O_RDONLY|O_NOFOLLOW,  0666);
+			if(fd_sous < 0){
+				perror("refus d'ouvrir un lien symbolique avec l'option \"-P\"");
+				exit(1);
+			}
+			else{
+				close(fd);
+				fd = fd_sous;
+			}
+			enlever_premier_dossier(param);
+		}
+		sprintf(dernier_sym, "%s", getenv("PWD"));
+		//une fois le dossier ouvert, appel de la fonction "pwd" qui mettra à jour la variable d'environement
+		
+		//cette fonction n'existe pas. Elle prendra en paramètre le descripteur de fichier du dossier actuel?
+		//TODO: ECRIRE CETTE FONCTION
+		//pwd(fd);
+		close(fd);
+
+	}
+	else{
+		//afficher les instruction d'utilisation
+		exit(1);
+	}
+	return 0;
+
+}
