3816eba030c647b36bcd45ec8cfced0393413a32
MUKAILA ALH SHITTU abdoulbasti <mukailaabdoulbassit@gmail.com>
2022-12-05 19:53:01 +0100

    plan pour les commandes externes

---
+++ commandsExterns/commandesExterns.c
@@ -0,0 +1,86 @@
+#include "constant.h"
+#include "commandsExterns.h"
+
+
+
+
+
+
+/*
+Gestion des argument ds commandes externes aux apssage aux commandes externes
+*/
+
+
+/*
+Recuperer une commandes et ces parametres à stocker dans un tableau de tableau
+*/
+char** recupererCommandeEtParametres()
+{
+
+}
+
+
+
+
+/*
+Execution de la commandes extern
+*/
+void executionCommandeExtern()
+{
+
+}
+
+/*
+Verifie si 
+*/
+int verifierEmplacement()
+{
+
+}
+
+
+/*
+Splite sur le character ':', recuperer les chaines et les stocker dans un tableau de tableau
+*/
+char** recupererRepertoirePATH(char* variablePath)
+{
+    
+}
+
+
+
+
+/*
+Execution de tous les autres commandes se trouvant à un emplacement donner.
+*/
+void otherExternalCommandes(char* nomExecutable)
+{
+
+}
+
+
+/*
+Exécuter l'ensemble des commandes(executable) se trouvant à l'emplacement /usr/bin/ du system
+    ->
+    ->
+    ->
+    ->
+*/
+
+void userBinExternalCommands()
+{
+
+}
+
+
+/*
+Execution de l'ensemble de tous les commandes externs
+    ->
+    ->
+    ->
+    ->
+*/
+void commandesExterns()
+{
+
+}
---
+++ commandsExterns/commandsExterns.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include "../constant.h"
