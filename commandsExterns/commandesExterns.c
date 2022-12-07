45ae0c83d3407d5337ff2dbaab1fb9ade217cc76
MUKAILA ALH SHITTU abdoulbasti <mukailaabdoulbassit@gmail.com>
2022-12-07 10:36:23 +0100

    test unitaire de certain fonctions...

--- commandsExterns/commandesExterns.c
+++ commandsExterns/commandesExterns.c
@@ -1,15 +1,46 @@
-#include "constant.h"
-#include "commandsExterns.h"
+/*#include "constant.h"
+#include "commandsExterns.h"*/
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/wait.h>
 
 
-/*
-Ajout de NULL à la fin de du tableau de tableau de la chaine recuperer par depuis le prompt
-*/
-void ajouterNULL(char** arguments)
+#define MAX_ARGS_NUMBER 4096
+#define MAX_ARGS_STRLEN 4096
+
+char** allocation()
 {
-    int taille = strlen(arguments);
-    arguments[taille] = NULL;
+    char tableauEspace[MAX_ARGS_STRLEN];
+    char** argument = (char**) malloc(MAX_ARGS_NUMBER * sizeof(char*));
+
+    for(int i = 0; i< MAX_ARGS_NUMBER; i++)
+    {
+        //argument[i] = (char*) malloc(sizeof(char) * (MAX_ARGS_STRLEN));
+        //argument[i] = tableauEspace;
+        argument[i] = NULL;
+    }
+    return argument;
 }
+
+/*void liberer(char* variable[])
+{
+    for(int i = 0; i<MAX_ARGS_NUMBER; i++)
+    {
+        free((char*)variable[i]);
+    }
+    free((char**)variable);
+}*/
+
+
 
 
 /*
@@ -17,24 +48,11 @@ gestion d'erreur, si le code de retour est -1 on affiche la dernière valeur de
 la variable errno
 */
 
-void gestionErreurEntier(char* nomFonction, int retourFonction)
+void gestionErreur(char* nomFonction)
 {
-    if(retourFonction == -1)
-    {
-        perror(nomFonction);
-        exit(1);
-    }
+    perror(nomFonction);
+    exit(1);
 }
-
-void gestionErreurPointeur(char* nomFonction, void* poiteurRetour)
-{
-    if(poiteurRetour == NULL)
-    {
-        perror(nomFonction);
-        exit(1);
-    }
-}
-
 
 
 /*
@@ -48,25 +66,63 @@ Recuperer une commandes et ces parametres à stocker dans un tableau de tableau.
 Reserver toujours une place de plus pour le NULL
 
 */
-char** recupererCommandeEtArguments(char* commandesEtArguments)
+//char** recupererCommandeEtArguments(char str[])
+char** recupererCommandeEtArguments(char strr[MAX_ARGS_STRLEN])
 {
-    char** arguments;
-    char* idChaineSuivant;
+    char** arguments = allocation();
+    //char* idChaineSuivant = (char*) malloc(MAX_ARGS_STRLEN);
     char* espace = " ";
     int compteur = 0;
 
-    idChaineSuivant = strtok(commandesEtArguments, espace);
+    //idChaineSuivant = strtok(commandesEtArguments, espace);
 
-    while (idChaineSuivant != NULL)
+    //char strr[] = "Geeks for Geeks Salut";
+    //char* strr = "Geeks for Geeks Salut";
+    char* tokenSuivant;
+    char* chainesArguments = strr;
+    //char* rest = "Geeks for Geeks Salut";
+    //char* rest = str;
+    
+    while ((tokenSuivant = strtok_r(chainesArguments, " ", &chainesArguments)))
     {
-        sprintf(arguments[compteur], "%s", idChaineSuivant);
-        idChaineSuivant = strtok(NULL, espace);
+        //char temporaire[MAX_ARGS_STRLEN] = tokenSuivant;
+        arguments[compteur] = tokenSuivant;
+        //printf("%s\n", tokenSuivant);
         compteur++;
     }
     arguments[compteur] = NULL;
     
+    //char*  test = strtok("ceci-est-un-test", "  - ");
+    
+    /*while (idChaineSuivant != NULL)
+    {
+        //sprintf(arguments[compteur], "%s", idChaineSuivant);
+        printf("%s\n", idChaineSuivant);
+        arguments[compteur] = idChaineSuivant;
+        idChaineSuivant = strtok(NULL, espace);
+        compteur++;
+    }*/
+    //arguments[compteur] = NULL;
+    
+    //return arguments;
     return arguments;
 }
+
+
+/*
+Execution de tous les autres commandes se trouvant à un emplacement donner.
+*/
+void executionCommandesExternesAutres(char** arguments)
+{
+    int retourExec  = execv(arguments[0], arguments);
+    char* exec = "execv";
+    if(retourExec == -1) 
+    {
+        printf("exection de %s a echoue \n", exec);
+        gestionErreur(exec);
+    }
+}
+
 
 /*
 Execution de la commandes extern
@@ -78,23 +134,14 @@ void executionCommandeExternes(char** arguments)
 {
     int retourExec = execvp(arguments[0], arguments);
     char* exec = "execvp";
+
     
     //Si l'execution d'une commande de Path echou on essaye alors celui se trouvant dans un autre emplacement
     if (retourExec == -1)
     {
+        printf("exection de %s a echoue \n", exec);
         executionCommandesExternesAutres(arguments);
     }
-}
-
-
-/*
-Execution de tous les autres commandes se trouvant à un emplacement donner.
-*/
-void executionCommandesExternesAutres(char** arguments)
-{
-    int retourExec  = execv(arguments[0], arguments);
-    char* exec = "execv";
-    gestionErreurEntier(exec, retourExec);
 }
 
 
@@ -105,12 +152,16 @@ Execution de l'ensemble de tous les commandes externs
     ->
     ->
 */
-void commandesExternes(char** arguments)
+void commandesExternes(char* chainePrompt)
 {
-    pid_t pid = fork();
+    //char* arguments[MAX_ARGS_NUMBER] = recupererCommandeEtArguments(chainePrompt);
+    char** arguments;
+    allocation(arguments);
     char* nomFonction = "fork";
+    pid_t pid = fork();
 
-    gestionErreurEntier(nomFonction, (int)pid);
+    if(pid == -1) { gestionErreur(nomFonction);}
+
     if (pid == 0)
     {
         executionCommandeExternes(arguments);
@@ -119,4 +170,41 @@ void commandesExternes(char** arguments)
     {
         wait(NULL);
     }
+}
+
+
+int main()
+{
+    char prompt[MAX_ARGS_STRLEN] = "hgjhkjlksfd autres choses et on va voir se que ça va donner";
+    char** commandes = allocation();
+    //Sans erreur : L'initialisation de l'eelment doit passer par un tableau initialement initialiser
+    /*char test[MAX_ARGS_STRLEN] = "test test2";
+    commandes[0] = test;
+    recupererCommandeEtArguments(commandes[0]);
+    printf("%s", commandes[0]);*/
+    /*
+    Core dump error
+    commandes[0] = "test test2";
+    recupererCommandeEtArguments(commandes[0]);
+    */
+    //printf("%s", commandes[0]);
+    int statusWait;
+    commandes = recupererCommandeEtArguments(prompt);
+    char nomFonction[] = "fork";
+    
+
+    pid_t pid = fork();
+    if(pid == -1) { gestionErreur(nomFonction); }
+    else if(pid == 0)
+    {
+        //void executionCommandeExternes(char** arguments)
+        //....
+    }
+    else
+    {
+        wait(&statusWait);
+    }
+    
+    free((char**)commandes);
+    return 0;
 }
--- commandsExterns/commandsExterns.h
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <string.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <stdio.h>
-#include "../constant.h"
