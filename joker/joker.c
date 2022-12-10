8a68c2718df99e6fe7bb0b7848e831587877e809
julien <julien.cartigny@etu.u-paris.fr>
2022-12-10 11:59:00 +0100

    cherche une étoile dans tous les args

--- joker/joker.c
+++ joker/joker.c
@@ -1,1 +1,31 @@
 #include "joker.h"
+
+int is_etoile_simple(char* argv){
+    for (size_t i = 0; i < strlen(argv); i++){
+        if(argv[i] == '*'){
+            return i;
+        }
+    }
+    return -1;    
+}
+
+char** joker(int argc, char** argv){
+    char** argv2;       //argv à renvoyer
+    int args_ajout = 0; //nbr d'arguments rajoutés
+    for (size_t i = 0; i < argc; i++){
+        int place = is_etoile_simple(argv[i]);
+        if(place != -1){
+
+        }else{
+            argv2[i + args_ajout] = argv[i];
+        }
+    }
+}
+
+int main(int argc, char** argv){
+    for (size_t i = 0; i < argc; i++){
+        printf("%ld: %d\n", i, is_etoile_simple(argv[i]));
+    }
+    
+    
+}
--- joker/joker.h
+++ joker/joker.h
@@ -1,2 +1,5 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+char** joker(int argc, char** argv);
