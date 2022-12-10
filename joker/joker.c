5398d2ba7345a62405f6d08174e27f6a340e4ca0
julien <julien.cartigny@etu.u-paris.fr>
2022-12-10 13:24:12 +0100

    debut recherche etoile simple

--- joker/joker.c
+++ joker/joker.c
@@ -1,31 +1,59 @@
 #include "joker.h"
 
-int is_etoile_simple(char* argv){
-    for (size_t i = 0; i < strlen(argv); i++){
-        if(argv[i] == '*'){
+int is_etoile_simple(char* arg){
+    for (size_t i = 0; i < strlen(arg); i++){
+        if(arg[i] == '*'){
             return i;
         }
     }
     return -1;    
 }
 
-char** joker(int argc, char** argv){
-    char** argv2;       //argv à renvoyer
-    int args_ajout = 0; //nbr d'arguments rajoutés
-    for (size_t i = 0; i < argc; i++){
-        int place = is_etoile_simple(argv[i]);
-        if(place != -1){
+int cherche_prefixe(char** argv2, char* arg, int place){
+    char path[PATH_MAX];
+    strcpy(path, getenv("PWD"));
+    char suffixe[MAX_ARGS_STRLEN];
+    strcpy(suffixe, arg + place + 1);
+    arg[place] = '\0';
+    strcat(path, "/");
+    strcat(path, arg);
+    path[strlen(path) - 1] = '\0';
 
+    printf("%s : %s\n", path, suffixe);
+
+    DIR * dir = opendir(path);
+    if(dir = NULL){
+        return -1;
+    }
+    struct dirent * entry;
+    printf("TEST\n");
+	while((entry = readdir(dir))){
+        printf("TEST2\n");
+        //printf("%s\n", entry->d_name);
+    }
+
+    return -1;
+}
+
+int joker(int argc, char** argv){
+    char* argv2[MAX_ARGS_NUMBER];       //copie argv
+    for (size_t i = 0; i < argc; i++){
+        argv2[i] = argv[i];
+    }
+    
+    int args_ajout = 0; //nbr d'arguments rajoutés
+    int place = -1;
+    for (size_t i = 0; i < argc; i++){
+        place = is_etoile_simple(argv2[i]);
+        if(place != -1){
+            args_ajout += cherche_prefixe(argv, argv2[i], place);
         }else{
-            argv2[i + args_ajout] = argv[i];
+            argv[i + args_ajout] = (char*) argv2[i];
         }
     }
+    return -1;
 }
 
 int main(int argc, char** argv){
-    for (size_t i = 0; i < argc; i++){
-        printf("%ld: %d\n", i, is_etoile_simple(argv[i]));
-    }
-    
-    
+    joker(argc, argv);
 }
--- joker/joker.h
+++ joker/joker.h
@@ -1,5 +1,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include "../constant.h"
 
-char** joker(int argc, char** argv);
+int joker(int argc, char** argv);
+
+
+/*
+ * prend une chaine de caractère et la position de la première étoile si il y en a une
+ * et renvoi -1 sinon
+*/
+int is_etoile_simple(char* argv);
